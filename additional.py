038cee10c2315476c92de5d930dc0f0c5ed096c8
JerryTheMouse <toperkas@gmail.com>
2023-08-21 04:57:51 +0600

    remove trash func

--- additional.py
+++ additional.py
@@ -1,11 +1,4 @@
 from aiogram.types import Message
-import text as t
 
 class Additions:
-    def log(text):
-        try:
-            print("LOG: " + text)
-        except:
-            print("ERR: Unable to log information.")
-
     msg_to_edit = Message
